fef2eee48252771f2bc3d34196ebb3475c8b2bdf
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-05-30 22:03:20 +0300

    Update sos_config.c

--- SOS/sos_config.c
+++ SOS/sos_config.c
@@ -1,1 +1,28 @@
-....
+#include"sos.h"
+
+Task_t SystemTasks[TASK_NUM]={
+	/*Task#1*/
+	{
+		.periodicity=300,
+		.TaskFunc=Func1,
+		.Copy_u8Priority=0;
+	},
+	/*Task#2*/
+	{
+		.periodicity=500,
+		.TaskFunc=Func2,
+		.Copy_u8Priority=1;
+	},
+	/*Task#3*/
+	{
+		.periodicity=INVALID_TASK,
+		.TaskFunc=INVALID_TASK,
+		.Copy_u8Priority=INVALID_TASK;
+	},
+	/*Idle task*/
+	{
+		.periodicity=50,
+		.TaskFunc=Idle_Func,
+		.Copy_u8Priority=TASK_NUM-1;
+	}
+};
